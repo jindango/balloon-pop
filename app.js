8cf60440f90cd7e156ce050a6a9c090d5dccd5e6
James <jamesdn12@gmail.com>
2021-02-22 21:46:20 -0500

    more updates

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 let balloons_popped = 0;
 let number_popped_to_win = 10;
 
-let my_variable = function (x) { return x +1; };
+function handleBalloonClick(e) {
+    e.target.style.backgroundColor = "#ededed";
+    e.target.textContent = "POP!";
+    balloons_popped = balloons_popped + 1;
+    e.target.removeEventListener('click', handleBalloonClick);
 
-document.addEventListener('click', function(e){
-    if (e.target.className === "balloon"){
-        e.target.style.backgroundColor = "#ededed";
-        e.target.textContent = "POP!";
-        balloons_popped = balloons_popped + 1;
-        e.target.removeEventListener('click', function() {} )
-        checkIfAllBalloonsPopped();
-    }   
-});
-
-function checkIfAllBalloonsPopped() {
     if (balloons_popped === number_popped_to_win) {
-        let gallery = document.getElementById('balloon-gallery');
-        let message = document.getElementById('you-win');
-        gallery.innerHTML = '';
-        message.style.display = 'block';
+        document.getElementById('balloon-gallery').innerHTML = '';
+        let message = document.getElementById('you-win').style.displaya = '';
     }
-};
+}
+
+let balloons = document.getElementsByClassName("balloon");
+for (let i = 0; i < balloons.length; i++ ) {
+    balloons[i].addEventListener('click', handleBalloonClick);
+}
